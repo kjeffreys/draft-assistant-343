5cc4292f34d1a4182fd2c68bad0d4aef78f6fa92
Kyle <kjeffreyscs@gmail.com>
2025-06-18 20:02:39 -0400

    Add import league page with routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to Fantasy Draft Assistant</h1>
       <p>Get ready to draft the perfect team!</p>
+      <Link to="/import-league">Import League</Link>
     </main>
   )
 }
